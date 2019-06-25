7a0066750537a6418cbede5dd4878936bea98501
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-06-25 07:12:35 -0700

    Create s3.tf

---
+++ terraform/s3.tf
@@ -0,0 +1,21 @@
+# S3 bucket config //-----------------------------------------------------------------
+
+resource "aws_s3_bucket" "examples3" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "${var.bucket_name}"
+  acl    = "private"
+ 
+  /*
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+  tags {
+    website = "true"
+  }  */
+}
