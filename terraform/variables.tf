263b303b6cf65f37c159900a1a69ccc4596b9ed0
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-06-25 07:08:12 -0700

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {}
+
+variable "ami" {}
+
+variable "instance_type" {}
+
+#variable "instance_size" {}
+
+variable "ssh_key" {}
+
+variable "owner" {}
+
+variable "ttl" {}
+
+variable "count" {}
