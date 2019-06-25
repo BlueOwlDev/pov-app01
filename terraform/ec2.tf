32d6f438a95ef0c186ed388ca1cca5da5c77942f
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-06-25 07:07:03 -0700

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,27 @@
+#Ec2 instance //---------------------------------------
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ec2_vm" {
+  count = "${var.count}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  
+# VPC Subnet
+subnet_id = "${aws_subnet.subnet-public-1a.id}"
+
+# Security Group
+vpc_security_group_ids = ["${aws_security_group.secgroup.id}"]
+
+# SSH key in AWS account
+key_name = "${var.ssh_key}"
+ 
+# tags
+tags {
+  Name = "ec2-${count.index}-${var.environment_name}"
+  owner = "${var.owner}"
+  TTL = "${var.ttl}"
+ }
+}
